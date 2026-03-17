de7f17956ac935b17e1b593e5dd8b6a25fd01f28
Seb47536 <a47536@alunos.ipb.pt>
2026-03-17 12:37:05 +0000

    Corrigido um erro no código

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
 
-    public float jumpForce = 10f;
+    public float jumpForce = 10;
     public float gravityModifier;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,9 +18,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
